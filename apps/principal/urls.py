2e6504ef2700dc5ce79ef58e2e9096bc22d1540f
AnaLuisaIA <analuisa.iavila@gmail.com>
2016-12-02 02:37:16 -0600

    Agregar HTML, captcha y archivos estáticos

--- apps/principal/urls.py
+++ apps/principal/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from apps.principal.views import index
+from apps.principal.views import index, mostrar_grafo
 
 urlpatterns = [
-    url(r'^$', index),
+    url(r'^$', index,name='principal'),
+    url(r'^grafo$', mostrar_grafo,name='grafo'),
 ]
